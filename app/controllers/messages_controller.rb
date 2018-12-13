1c9809250073f474ea96b449fac905b46b624536
飯塚嘉宣 <rainetokomugi@gmail.com>
2018-12-13 17:23:39 +0900

    edit_messages_controller.rb
    # WHAT
    
    # WHY
    最新メッセージを上部表示する機能が必要ない為。
    メッセージが保存できなかった場合の処理を追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,22 +4,27 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user).order('id DESC')
+    @messages = @group.messages.includes(:user)
   end
 
   def create
     @message = @group.messages.new(message_params)
-    @message.save
-    respond_to do |format|
-      format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
-      format.json
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+        format.json
+      end
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id:current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group
