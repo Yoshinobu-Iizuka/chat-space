84afe0700937717cc53bec103d678a01b9ee61fa
飯塚嘉宣 <rainetokomugi@gmail.com>
2018-12-15 16:33:28 +0900

    edit_users_controller
    # WHAT
    記述の追加
    
    # WHY
    ユーザー検索時にログインユーザー名が表示されない様にする為。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    @users = User.where.not(id: current_user.id)
     respond_to do |format|
       format.json
     end
