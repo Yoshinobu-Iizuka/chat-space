b3754229878f582dbe85822157576a7d6cd444bc
飯塚嘉宣 <rainetokomugi@gmail.com>
2018-12-17 21:49:46 +0900

    add_message.js
    # WHAT
    自動更新を行う記述を追加
    
    # WHY
    他のユーザーの投稿を非同期通信を使い自動更新で表示する為。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,12 @@
 $(document).on('turbolinks:load', function() {
+  var chatMain = '.chat-main__body'
   var buildHTML = function(message) {
   if ( message.image ) {
     var html =
     `<div class="chat-main__body__name">${message.user_name}</div>
     <div class="chat-main__body__name__timestamp">${message.created_at}</div>
-    <div class="chat-main__body__comment">${message.content}</div>
-    <div class="image">${message.image}</div>`
+    <div class="chat-main__body__comment">${message.content}<img class="image" src="${message.image}"></div>
+    <div data-message-id="${message.id}"></div>`
   } else {
     var html =
      `<div class="chat-main__body__name">${message.user_name}</div>
@@ -37,5 +38,29 @@ $(document).on('turbolinks:load', function() {
       });
       return false;
   });
-})
+  setInterval(function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+  var id = $('.chat-main__body__name').last().data('messageId');
+  $.ajax({
+    url: location.href,
+    type: "GET",
+    dataType: 'json',
+    data: {id: id}
+    })
+    .done(function(messages){
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML += buildHTML(message);
+        console.log(message);
+      });
+      $(chatMain).append(insertHTML);
+      $(chatMain).animate({scrollTop: $(chatMain)[0].scrollHeight}, 2000, 'swing');
+    })
+    .fail(function(data){
+      alert('自動更新に失敗しました');
+    });
+  } else {
+    clearInterval(interval);
+    }} , 5000 );
+});
 
